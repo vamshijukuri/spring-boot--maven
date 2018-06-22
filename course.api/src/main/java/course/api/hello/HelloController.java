8048f5841900a762e969d20e31c2164c35bb19f0
vamshijukuri <vamshi2153@gmail.com>
2018-06-22 16:44:57 +0530

    first commit

---
+++ course.api/src/main/java/course/api/hello/HelloController.java
@@ -0,0 +1,13 @@
+package course.api.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController{
+     
+	@RequestMapping("/hello")
+	 public String sayhi(){
+		 return"Welcome";
+}
+}
---
+++ course.api/src/main/java/course/api/hello/TopicController/TopicService.java
@@ -0,0 +1,47 @@
+package course.api.hello.TopicController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics= new ArrayList<>(Arrays.asList(
+			new Topic("Spring","Spring Framework","a large spring framework description"),
+			new Topic("java","Java 8","Platform indipendent language description"),
+			new Topic("SQL","SQL 11g","Database description"),
+			new Topic("Angular","Angular 4","Front End development description")
+		    ));
+    public List<Topic> getAllTopics(){
+		return topics;
+    }
+
+public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst.get();
+	}
+
+public void addTopic(Topic topic) {
+	topics.add(topic);
+	
+}
+
+public void updateTopic(String id, Topic topic) {
+	for(int i=0; i< topics.size(); i++) {
+		Topic t= topics.get(i);
+		if(t.getId().equals(id)) {
+			topics.set(i, topic);
+			return;
+		}
+		
+	}
+	
+}
+
+public void deleteTopic(String id) {
+	topics.removeIf(t -> t.getId().equals(id));
+}
+
+}
